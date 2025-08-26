2a305d0d22e38ff882b2a48744d7d0611516255e
root10 <hakersgenie@gmail.com>
2025-08-26 20:19:30 +0200

    likuta

--- Wallet.cpp
+++
@@ -1,8 +0,0 @@
-#include "Wallet.h"
-#include "Utils.h"
-
-Wallet::Wallet(const std::string& name) {
-    privateKey = Utils::generatePrivateKey(name);
-    publicKey = Utils::generatePublicKey(privateKey);
-    address = Utils::generateAddress(publicKey);
-}
